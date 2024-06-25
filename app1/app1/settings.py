67ae55a6deb315d07dd9cbe2bade79da8aa87fa0
Alexis <166058737+Alescobarc@users.noreply.github.com>
2024-06-25 01:52:29 -0400

    Arreglo settings

--- app1/app1/settings.py
+++ app1/app1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h$*^^rya3_o@b74w)c+p%wg$vg4+j#k-awiw!*rh8jjag94n-c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
